c89bd811edb5aea115c4b057cb6ad5e268893169
davis opondo <opondodav201994@gmail.com>
2018-12-10 17:01:49 +0300

    add js functions if and else statements

--- script.js
+++ script.js
@@ -13,3 +13,14 @@ function scoreCalculator(userValue) {
 $(function() {
     $("#start-btn").click(function(event) {
       event.preventDefault();
+      var userFirstName = $("#firstName").val();
+    var userSecondName = $("#secondName").val();
+    if(userFirstName.length === 0 || userSecondName.length === 0) {
+      $("#no-name").show();
+      $("#firstName").addClass("enter-name");
+      $("#secondName").addClass("enter-name");
+    } else {
+      $("#intro").hide();
+      $("#form1").show();
+    }
+  })
