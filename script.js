8e13f8f0392a93f4b91daae780ab211e641b80dd
davis opondo <opondodav201994@gmail.com>
2018-12-10 16:04:53 +0300

    add javascript functionality

--- script.js
+++ script.js
@@ -9,3 +9,19 @@ function scoreCalculator(userValue) {
     }
   });
 }
+
+
+$(function() {
+    $("#start-btn").click(function(event) {
+      event.preventDefault();
+      var userFirstName = $("#firstName").val();
+      var userSecondName = $("#secondName").val();
+      if(userFirstName.length === 0 || userSecondName.length === 0) {
+        $("#no-name").show();
+        $("#firstName").addClass("enter-name");
+        $("#secondName").addClass("enter-name");
+      } else {
+        $("#intro").hide();
+        $("#form1").show();
+      }
+    })
