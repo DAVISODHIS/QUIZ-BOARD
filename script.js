8aeea2699f609c7c3d4815f00a454abb09897f49
davis opondo <opondodav201994@gmail.com>
2018-12-10 17:47:52 +0300

    add jquery link and editing html structure

--- script.js
+++ script.js
@@ -10,45 +10,45 @@ function scoreCalculator(userValue) {
         }
     });
 }
-$(function() {
-    $("#start-btn").click(function(event) {
-      event.preventDefault();
-      var userFirstName = $("#firstName").val();
-    var userSecondName = $("#secondName").val();
-    if(userFirstName.length === 0 || userSecondName.length === 0) {
-      $("#no-name").show();
-      $("#firstName").addClass("enter-name");
-      $("#secondName").addClass("enter-name");
-    } else {
-      $("#intro").hide();
-      $("#form1").show();
-    }
-  })
+$(function () {
+    $("#start-btn").click(function (event) {
+        event.preventDefault();
+        var userFirstName = $("#firstName").val();
+        var userSecondName = $("#secondName").val();
+        if (userFirstName.length === 0 || userSecondName.length === 0) {
+            $("#no-name").show();
+            $("#firstName").addClass("enter-name");
+            $("#secondName").addClass("enter-name");
+        } else {
+            $("#intro").hide();
+            $("#form1").show();
+        }
+    })
 
-  $("#arrow-right").click(function() {
-    $("#form1").hide();
-    $("#form2").show();
-  })
-  $("#arrow-left").click(function() {
-    $("#form1").show();
-    $("#form2").hide();
-  })
-  $("#submit").click(function(event) {
-    event.preventDefault();
-    $("#score").show();
-    $("#form2").hide();
-    scoreCalculator();
-    var userFirstName = $("#firstName").val();
-    var userSecondName = $("#secondName").val();
-    if(sum <= 30) {
-      $(".name-display").text(`Nice try ${userFirstName} ${userSecondName},`);
-    } else {
-      $(".name-display").text(`Congratulations ${userFirstName} ${userSecondName},`);
-    }
-    $(".scoreDisplay").text(sum + "/60");
-  })
-  $("#retake").click(function(event) {
-    event.preventDefault();
-    location.reload();
-  })
+    $("#arrow-right").click(function () {
+        $("#form1").hide();
+        $("#form2").show();
+    })
+    $("#arrow-left").click(function () {
+        $("#form1").show();
+        $("#form2").hide();
+    })
+    $("#submit").click(function (event) {
+        event.preventDefault();
+        $("#score").show();
+        $("#form2").hide();
+        scoreCalculator();
+        var userFirstName = $("#firstName").val();
+        var userSecondName = $("#secondName").val();
+        if (sum <= 30) {
+            $(".name-display").text(`Nice try ${userFirstName} ${userSecondName},`);
+        } else {
+            $(".name-display").text(`Congratulations ${userFirstName} ${userSecondName},`);
+        }
+        $(".scoreDisplay").text(sum + "/60");
+    })
+    $("#retake").click(function (event) {
+        event.preventDefault();
+        location.reload();
+    })
 })
